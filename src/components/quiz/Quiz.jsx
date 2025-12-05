6fcf732c0dd8a5ef84c1eccb858d5e9af03e0123
maynul-cs <maynulislammishu@gmail.com>
2025-12-05 19:09:14 +0600

    added css style

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -3,8 +3,18 @@ import './Quiz.css'
 
 const Quiz = () => {
   return (
-    <div>
-        Quiz Component
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Which device is required for the Internet connection?</h2>
+        <ul>
+            <li>Modem</li>
+            <li>Router</li>
+            <li>LAN Cable</li>
+            <li>Pen Drive</li>
+        </ul>
+        <button>Next</button>
+        <div className="index">1 of 5 questions</div>
     </div>
   )
 }
